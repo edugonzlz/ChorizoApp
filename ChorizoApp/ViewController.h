9578fd56638484b756f94ba67a2c33e9171fda86
Edu Gonzalez <fonologico@gmail.com>
2015-11-21 11:13:38 +0100

    Initial Commit

---
+++ ChorizoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChorizoApp
+//
+//  Created by Edu González on 21/11/15.
+//  Copyright © 2015 Edu González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChorizoApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChorizoApp
+//
+//  Created by Edu González on 21/11/15.
+//  Copyright © 2015 Edu González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
